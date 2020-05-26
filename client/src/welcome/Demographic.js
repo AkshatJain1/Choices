30c69e16ea38551da9446d9af778fba8e1881601
Akshat Jain <akshatj@berkeley.edu>
2020-05-26 02:27:15 -0500

    added modals, animations, fixed styling on preferences, adding rating system

--- client/src/welcome/Demographic.js
+++ client/src/welcome/Demographic.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Dimensions} from 'react-native';
-import { Input, Card, Text, Datepicker, IndexPath, Select, SelectItem, Button } from '@ui-kitten/components';
+import {View, StyleSheet, Dimensions, Animated, Easing} from 'react-native';
+import { Input, Card, Text, Datepicker, IndexPath, Select, SelectItem, Button, Modal } from '@ui-kitten/components';
 
 const WIDTH = Dimensions.get('window').width;
 const HEIGHT = Dimensions.get('window').height;
 
+const AnimatedModal = Animated.createAnimatedComponent(Modal)
+
 export const RACES = [
+    'Select',
     'White', 
     'Black or African American', 
     'American Indian or Alaska Native',
@@ -14,6 +17,7 @@ export const RACES = [
 ];
 
 export const GENDERS =[
+    'Select',
     'Male',
     'Female',
     'Other'
@@ -35,7 +39,12 @@ const styles = StyleSheet.create({
         marginVertical: 8,
     },
 });
-  
+
+const Header = (props) => (
+    <View {...props}>
+      <Text category='h5'>Let's get to know you!</Text>
+    </View>
+);
 
 export default class Demographic extends Component {
     constructor(props) {
@@ -46,13 +55,65 @@ export default class Demographic extends Component {
             'birthday': '',
             'raceIndex': new IndexPath(0),
             'genderIndex': new IndexPath(0),
+            'modal_pos': new Animated.Value(100),
+            'modalVisible': true,
         };
+    }
+
+    componentDidMount() {
+        this.openModal();
+    }
+
+    onSubmit = () => {
+        let data = {};
+        if (this.state.name !== '') {
+            data.name = this.state.name;
+        }
+        if (this.state.birthday !== '') {
+            console.log(this.state.birthday);
+            data.birthday = this.state.birthday;
+        }
+        if (this.state.raceIndex.row !== 0) {
+            data.race = RACES[this.state.raceIndex.row];
+        }
+        if (this.state.genderIndex.row !== 0) {
+            data.gender = RACES[this.state.genderIndex.row];
+        }
+
+        this.props.navigation.navigate('Preferences', data)
+    }
+
+    openModal = () => {
+        let val = this.state.modal_pos;
+
+        Animated.timing(val, {
+            toValue: 0,
+            easing: Easing.bounce,
+            duration: 1200
+        }).start();
+    }
+
+    closeModal = () => {
+        let val = this.state.modal_pos;
+
+        Animated.timing(val, {
+            toValue: -40,
+            duration: 300
+        }).start(() => {
+            Animated.timing(val, {
+                toValue: 150,
+                duration: 500,
+            }).start(() => {
+                this.setState({modalVisible: false})
+            });
+        });
+
     }
     
     render() {
         return (
             <View style={styles.container}>
-                <View style={{flex: 1}}>
+                <View style={{flex: 1, alignContent: 'center', justifyContent: 'center'}}>
                     <Text category='h2'
                         style={{color: '#FFFCE4', padding: 30, fontWeight: 'bold'}}>
                         Demographics
@@ -73,7 +134,7 @@ export default class Demographic extends Component {
                             style={{marginTop: 20}} 
                             label='Birthday'
                             date={this.state.birthday}
-                            onSelect={birthday => this.setState({ birthday })}
+                            onSelect={birthday => this.setState({ birthday: birthday.getTime() })}
                         />
 
                         <Select
@@ -107,10 +168,28 @@ export default class Demographic extends Component {
                         position: 'absolute',
                         bottom: 100
                     }}>
-                    <Button status='primary' size='large' onPress={() => this.props.navigation.navigate('Preferences', this.state)}>
+                    <Button status='primary' size='large' onPress={this.onSubmit}>
                         Continue
                     </Button>
                 </View>
+
+                <AnimatedModal
+                style={{marginRight: '10%', marginTop: this.state.modal_pos.interpolate({
+                    inputRange: [0, 100],
+                    outputRange: ['0%', '100%']
+                })}}
+                visible={this.state.modalVisible}
+                backdropStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+                onBackdropPress={this.closeModal}>
+                <Card disabled={true} header={Header}>
+                    <Text category='p2'>Luckily, we promise to never release or expose our users' personal data and take extreme technological measures to ensure this.</Text>
+                    <View style={{ marginTop: '10%' }}>
+                        <Button style={{marginHorizontal: 2}} onPress={this.closeModal}>
+                            Got it!
+                        </Button>
+                    </View>
+                </Card>
+            </AnimatedModal>
             </View>
         );
     }
