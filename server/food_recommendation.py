6cf7e3ff71e863c0221d3738bc478f59097e362d
Pranav Krishnamoorthi <pranav01krishna@berkeley.edu>
2020-05-28 17:52:27 -0700

    Finished Preliminary Testing for Traversal, using sigmoid functions for heuristic now

--- server/food_recommendation.py
+++ server/food_recommendation.py
@@ -1,24 +1,37 @@
 import collections
 import json
 import sys
+import os
 import math
 #NOTE: THE RECOMMENDATION SHOULD NOT ONLY BE WEIGHTED BY DISTANCE FROM USER, BUT MUCH MORE SO BY THE DIFFERENCE IN THE CURRENTMENUWEIGHT AND THE THE WEIGHT OF THE USER
+#NOTE: THE DISTANCE FACTOR FUNCTION SHOULD BE A SIGMOID FUNCTION
 
+#NOTE: CHANGE THIS BASED ON WHAT IS THE BEST WAY TO STORE THE EDGE WEIGHTS OF USER TO ITEMS
+ratings_scale = 5
 L = 5
 K = 5
 limit_users = 10
 limit_food_items = 10   #calcula
 #distancefactor
-dist_inc_factor = 1
-dist_dec_factor = 5
-#incrase to have more effect by the distance
 
-dist_buffer = 1
-#controls how much distance needs to be to start making a large effect on the weight
+#controls (provide description)
+#have to tune, can possibly tune through a neural network type structure)
+dist_user_buffer = 3
+dist_user_growth = 2
 
-num_weight_inc_factor = 1
-num_weight_dec_factor = 1
-#how much affect that the amount of times we've run into a menu item and added it through preferences
+dist_user_item_buffer = 5
+dist_user_item_growth = 5
+
+user_item_pref_buffer = 1/2
+user_item_pref_growth = 15
+
+dist_menu_weight_buffer = 2
+menu_weight_growth = 5
+
+user_sim_buffer = 0
+user_sim_growth = 5
+
+
 
 #d
 #might need to include foodWeights in actual function iself
@@ -47,31 +60,43 @@ num_weight_dec_factor = 1
 
                   #node is a food item
 #NOTE: CAREFUL WITH FIELDS NOT UPDATING WITH FUNCTION CALL
-
-
-db = {}
-with open('server/traversal_test_5.json') as f:
-    db = json.load(f)
+#
+#
+# db = {}
+#
+# with open('traversal_test_5.json') as f:
+#     db = json.load(f)
 
 currentUserMenuWeights = {}
 currentMenuCount = {}
 dist_from_curr_user = {}
+db = {}
 #menu_items are id's of the menu items
-def user_item_traversal(curr_user_id, menu_items):
+def user_item_traversal(curr_user_id, menu_items, relpath):
+
+    global db
+    with open(relpath) as f:
+        db = json.load(f)
+
+
     global currentUserMenuWeights
     global currentMenuCount
     global UserMenuRatings
+    global dist_from_curr_user
     currentUserMenuWeights = {}
     currentMenuCount = {}
     UserMenuRatings = {}
     #NOTE: this user menu ratings thing may/may not be useless
     dist_from_curr_user = {}
 
+
     latest_menu_item = None
+    prev_food_item = None
 
     fringe_deque = collections.deque()
     fringe_deque.append(curr_user_id)
 
+    similar_ext_mul = 1
     user_marked = {}
     menu_item_marked = {}
     current_food_item_marked = {}
@@ -88,6 +113,10 @@ def user_item_traversal(curr_user_id, menu_items):
 
 
     #set the menu_items all to 0
+    # for menu_item in menu_items:
+    #     currentUserMenuWeights[menu_item] = 0
+
+    #set the menu items all to 1
     for menu_item in menu_items:
         currentUserMenuWeights[menu_item] = 0
 
@@ -108,23 +137,40 @@ def user_item_traversal(curr_user_id, menu_items):
 
         elif db[node_id]['info']['type'] == 'user':
             for menu_item in menu_items:  #need to mark the menuitem if it was traversed
-                #check to see if user is the current user or if the user has eaten the menu item or not
-                if curr_user_id == node_id or menu_item in db[node_id]['food_item_connections']:
-                    if not menu_item_marked.get(menu_item, False):
-                        fringe_deque.append(menu_item)
-                        menu_item_marked[menu_item] = True
-                        dist_from_curr_user[menu_item] = dist_from_curr_user[node_id] + 1
+                #check to see if the user has eaten that menu item
+                if menu_item not in db[node_id]['food_item_connections']:
+                    db[node_id]['food_item_connections'][menu_item] = -100
+                    db[menu_item]['user_connections'][node_id] = -100
 
-                        print(menu_item)
-                        print(dist_from_curr_user[menu_item])
+                #check to see if the menu item has been marked before before traversring
+                if not menu_item_marked.get(menu_item, False):
+                    fringe_deque.append(menu_item)
+                    menu_item_marked[menu_item] = True
+                    dist_from_curr_user[menu_item] = dist_from_curr_user[node_id] + 1
+
+                    print(menu_item)
+                    print(dist_from_curr_user[menu_item])
                     # currentUserMenuWeight[food_item_id] = curr_user_item_pref(curr_user_id, user_id, food_item_id, weightSoFar)
                     #dist_from_curr_user[node_id] = dist_from_curr_user[menu_item] + 1
       #IDEA TO BE IMPLEMENTED: For the extended food item algorithm. As we go through the extended food item list, we keep track of the items,
       # and once we reach a user node, we go through the list of items we have added and add the user id to those. The connection will have some magnitude edge value as well
 
         elif db[node_id]['info']['type'] == 'food_item':
+
+
             if node_id in menu_items:
                 latest_menu_item = node_id
+            else:
+                #this extra info storing element only gets added if acutal node is an extended food item
+                similar_ext_mul, prev_food_item = fringe_deque.popleft()
+
+            #this if structure is for calculating the similarity from previous food item
+            if prev_food_item:
+                dist_curr_to_item = dist_from_curr_user.get(node_id, 0)
+                similar_ext_mul = item_item_similarity(dist_curr_to_item, node_id, prev_food_item, similar_ext_mul)
+            else:
+                similar_ext_mul = 1
+
             for adj_node_id in db[node_id]['user_connections']:
             #similarity should be done here, when the user is connected, not when the user is removed
             #will figure out the specifics later\
@@ -134,34 +180,79 @@ def user_item_traversal(curr_user_id, menu_items):
                     dist_from_curr_user[adj_node_id] = dist_from_curr_user[node_id] + 1
 
 
-                dist_curr_to_user = dist_from_curr_user.get(adj_node_id, 0)
-                print("Dist curr to user ", dist_curr_to_user)
-                currentUserMenuWeights[node_id] = curr_user_item_pref(dist_curr_to_user, curr_user_id, adj_node_id, node_id, latest_menu_item)
+                currentUserMenuWeights[latest_menu_item] = curr_user_item_pref(curr_user_id, adj_node_id, node_id, latest_menu_item, similar_ext_mul)
+
+
+
                 if adj_node_id not in UserMenuRatings:
                     UserMenuRatings[adj_node_id] = {}
                 elif node_id not in UserMenuRatings[adj_node_id]:
                     #warning: redudant computation here as computed in the curr user pref funciton already
                     UserMenuRatings[adj_node_id][node_id] = obj_user_item_pref(adj_node_id, node_id)
+
+
+
              #then go into the other food nodes for similar items that the user has had before
             if node_id in menu_items:
+
                 current_food_item_marked.clear()
 
+                # NOTE: make sure we need to be marking this menu item node
+                current_food_item_marked[node_id] = True
+
+                # checks to see if there is a previous food item (which is the extended food item) to modify similarity multiplier, otherwise multiplier is 1
+
                 for adj_node_id in db[node_id].get('food_item_connections', {}):
-                    #CODE INSERT HERE: marking needs to be done
-                    current_food_item_marked[adj_node_id] = True
+                    # current_food_item_marked[adj_node_id] = True
+                    #start the dfs by adding all the adjacent nodes, each one preceded by the preceding node and the similar_ext_mul up to that point
                     if adj_node_id not in menu_items:
+                        fringe_deque.appendleft((similar_ext_mul, node_id))
                         fringe_deque.appendleft(adj_node_id)
                         dist_from_curr_user[adj_node_id] = dist_from_curr_user[node_id] + 1
 
-                        #check for whether the food item is in the menu
+
+                # this is for the case where no nodes are appended and we never go to extended food item zone
+                prev_food_item = None
+                # check for whether the food item is not in the menu
             else:
-              if not current_food_item_marked[node_id]:
-                  #do similarity between items test here
-                  current_food_item_marked[node_id] = True
-                  for adj_node_id in db[node_id]['food_item_connections']:
-                      if not current_food_item_marked[adj_node_id]:
-                          fringe_deque.appendleft(adj_node_id)
-                          dist_from_curr_user[adj_node_id] = dist_from_curr_user[node_id] + 1
+
+                #need to make srure the item hasn't been traversed already
+                if not current_food_item_marked.get(node_id, False):
+
+                    # do similarity between items test here
+                    # checks to see if there is a previous food item (which is the extended food item) to modify similarity multiplier, otherwise multiplier is 1
+                    # if prev_food_item:
+                    #     dist_curr_to_item = dist_from_curr_user.get(node_id, 0)
+                    #     similar_ext_mul = item_item_similarity(dist_curr_to_item, node_id, prev_food_item,
+                    #                                            similar_ext_mul)
+                    # else:
+                    #     similar_ext_mul = 1
+                    current_food_item_marked[node_id] = True
+
+                    # if(not prev_food_item):
+
+
+
+                    #CONTINUE DFS LOOKING FOR SIMILAR FOOD ITEMS (WILL SET LIMIT)
+                    #make sure we limit the number of adjacent nodes
+                    #MAKE SURE WE CHECK FOR MENU ITEMS FIRST
+                    for adj_node_id in db[node_id]['food_item_connections']:
+                        if not current_food_item_marked.get(adj_node_id, False):
+                            # if debug_step == 7:
+                            #     print("adjacent food node ", adj_node_id)
+                            #     print('slkdjflksdjflksdjlfksjdlk')
+                            #     sys.exit(1)
+                            # else:
+                            #     sys.exit(1)
+                            if adj_node_id not in menu_items:
+                                fringe_deque.appendleft((similar_ext_mul, node_id))
+                                fringe_deque.appendleft(adj_node_id)
+
+                            else:
+                                fringe_deque.appendleft(adj_node_id)
+
+                            dist_from_curr_user[adj_node_id] = dist_from_curr_user[node_id] + 1
+
 
         print("Current User Menu Weights: ")
         print(currentUserMenuWeights)
@@ -174,41 +265,101 @@ def user_item_traversal(curr_user_id, menu_items):
 
 #Testing functions, will greatly develop in futuredist_from_curr_user = {}
 #pass the currentUserMenuWeights as a paramaRter
-def curr_user_item_pref(distance, curr_user_id, user_id,food_item_id, latest_menu_item):
+def curr_user_item_pref(curr_user_id, user_id,food_item_id, latest_menu_item, similar_ext_mul):
         #Thoughts, perhaps we should do it so that the similarity will be based on the
         global currentMenuCount
+        global dist_from_curr_user
+
         if latest_menu_item not in currentMenuCount:
             currentMenuCount[latest_menu_item] = 0
-
-        distance_based_pref = dist_based_user_item_pref(distance, user_id, food_item_id)
-
-        if distance_based_pref != 0:
-            currentMenuCount[latest_menu_item] += 1
-            weightToAdd = (user_similarity(curr_user_id, user_id) * distance_based_pref) * (num_weight_dec_factor / currentMenuCount[latest_menu_item]*num_weight_inc_factor)
         else:
-            weightToAdd = 0
+            print(latest_menu_item)
 
-        return currentUserMenuWeights.get(food_item_id, 0) + weightToAdd
+        dist_curr_to_item = dist_from_curr_user.get(food_item_id, 0)
+        dist_curr_to_user = dist_from_curr_user.get(user_id, 0)
+
+        print("Dist curr to user ", dist_curr_to_user)
+
+
+        #NOTE NEED TO CHANGE THIS SO THERE IS DISTANCE FUNCTION FOR BOTH ITEM TO USER AND USER TO USER
+
+        dist_user_based_pref = dist_based_curr_user_pref(dist_curr_to_user, curr_user_id, user_id)
+        dist_item_based_pref = dist_based_user_item_pref(dist_curr_to_item, user_id, food_item_id)
+        num_menu_item_count = num_menu_item_so_far(currentMenuCount[latest_menu_item], dist_menu_weight_buffer, menu_weight_growth)
+
+        currentMenuCount[latest_menu_item] += 1
+        weightToAdd = 1
+        print("DIST USER BASED PREF ", dist_user_based_pref)
+        print("DIST ITEM BASED PREF ", dist_item_based_pref)
+        print("NUM MENU ITEM COUNT ", num_menu_item_count)
+        weightToAdd *= dist_user_based_pref * dist_item_based_pref * num_menu_item_count
+        print("WEIGHT TO ADD SO FAR ", weightToAdd)
+        # weightToAdd *= (num_weight_dec_factor / currentMenuCount[latest_menu_item]*num_weight_inc_factor)
+        weightToAdd *= similar_ext_mul
+
+
+        weightSoFar = currentUserMenuWeights.get(latest_menu_item, 0)
+
+        #no connection whatsoever, make so its as if there is no change.
+        #NOTE this will be critical to figuring out how to stop (like if there are too many 0s)
+        if weightToAdd < 0.01:
+            return weightSoFar
+        elif weightSoFar == 0:
+            return weightToAdd
+        else:
+            return weightSoFar * weightToAdd
+
+
+def num_menu_item_so_far(menu_item_count, dist_menu_weight_buffer, menu_weight_growth):
+    return inverse_sigmoid(menu_item_count, dist_menu_weight_buffer, menu_weight_growth)
 
 #How much a particular user prefers an item, and weighted by how far away this preference is from the obj
-def  dist_based_user_item_pref(dist_curr_to_user, user_id, food_item_id):
+def  dist_based_user_item_pref(dist_curr_to_item, user_id, food_item_id):
 
-    print("dist from " + user_id, dist_curr_to_user)
-    return obj_user_item_pref(user_id, food_item_id) * (dist_dec_factor / ((dist_curr_to_user + dist_buffer)*dist_inc_factor + 1))
+    # print("dist from user to item " , inverse_sigmoid(dist_curr_to_item, dist_item_buffer, dist_item_growth))
+    print("obj _user item pref", obj_user_item_pref(user_id, food_item_id))
 
+
+    # return obj_user_item_pref(user_id, food_item_id) * (dist_dec_factor / ((dist_curr_to_item + dist_buffer)*dist_inc_factor + 1))
+    return obj_user_item_pref(user_id, food_item_id)  * inverse_sigmoid(dist_curr_to_item, dist_user_item_buffer, dist_user_item_growth)
+
+
+#change the factors that do this to user user factors
+def dist_based_curr_user_pref(dist_curr_to_user, curr_user_id, user_id):
+    print("diff curr to user ", curr_user_id, user_id,dist_curr_to_user)
+    # return user_similarity(curr_user_id, user_id) * (dist_user_dec_factor / ((dist_curr_to_user + dist_buffer)*dist_user_inc_factor + 1))
+    print("DISTANCELKDJLKFLDKFJLKSDJFLKJSDLKFJLKSDF_____DISTANDCE")
+    print(user_id)
+    print(curr_user_id)
+    print("DIST INV SIGMOID ", inverse_sigmoid(dist_curr_to_user, dist_user_buffer, dist_user_growth))
+    return user_similarity(curr_user_id, user_id) * inverse_sigmoid(dist_curr_to_user, dist_user_buffer, dist_user_growth)
+
+def inverse_sigmoid(x,inflection_point=0, growth=1):
+    return 1 / (1 + math.exp(growth * (x - inflection_point)))
+
+def sigmoid(x, inflection_point=0, growth=1):
+    return 1 / (1 + math.exp((-1 * growth) * (x - inflection_point)))
 #The objective preference of the user to an item
 def obj_user_item_pref(user_id, food_item_id):
-    return db[user_id]['food_item_connections'].get(food_item_id, 0)
+
+
+    objective_pref = db[user_id]['food_item_connections'].get(food_item_id, -100)
+    print("Objective_pref ", objective_pref)
+    return sigmoid((db[user_id]['food_item_connections'].get(food_item_id, -100) / ratings_scale), user_item_pref_buffer, user_item_pref_growth)
+
 
 #how similar a user is to another
 def user_similarity(user_id_1, user_id_2):
     if user_id_1 == user_id_2:
         return 1
     print("Similarity:" )
-    print(db[user_id_1]['user_connections'].get(user_id_2, 0))
-    return db[user_id_1]['user_connections'].get(user_id_2, 0)
+    print(db[user_id_1]['user_connections'].get(user_id_2, -100))
+    return sigmoid(db[user_id_1]['user_connections'].get(user_id_2, -100), user_sim_buffer, user_sim_growth)
 #functions that will be determined later
 # def user_item_pref(user_id, item_id):
 #     pass
-def item_item_similarity(item_id_1, item_id_2):
-    pass
+
+
+#this function will be complicated enough
+def item_item_similarity(dist_curr_to_item, item_id_1, item_id_2, similar_ext_mul):
+    return similar_ext_mul * (db[item_id_1]['food_item_connections'].get(item_id_2, -100))
--- server/test_food_recommendation.py
+++ server/test_food_recommendation.py
@@ -9,41 +9,55 @@ from food_recommendation import user_item_traversal
 #Assume that all the items are on the menu, and that the user has only had that particular food item at that point of time
 #Assume that we are not considering similar items (including items that are just the item without any restaurant)
 #Maximum Levels Traversed: 1
-# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6"])
+# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6"], 'traversal_test_1.json')
+# Final Current User Menu Weights
+# {'Hot Dog': 0.6981909851282292, 'Chicken Sandwhich': 0.26182161942308596, 'Cheeseburger': 0.26182161942308596, 'Hamburger': 0.20945729553846879, 'Chili Cheese Fries': 0.19200252091026307}}
 
 #NOTE:raversal Test 2
 #Same as Traversal Test 1
 #Will also add in Another User, Akshat, who unrealistically has a similarity of "1", or exactly the same as currentUser, Pranav
 #Akshat will have had Pizza, an item that Pranav has not already had
 #Maximum Levels Traversed: 2
-# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"])
+# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"], 'traversal_test_2.json')
 #output:
 # Final Current User Menu Weights
-# {'Hot Dog': 10.0, 'Pizza': 5.0, 'Chicken Sandwhich': 3.75, 'Cheeseburger': 3.75, 'Hamburger': 3.0, 'Chili Cheese Fries': 2.75}
+# {'Hot Dog': 0.6981909851282292, 'Pizza': 0.6164889243616287, 'Chicken Sandwhich': 0.26182161942308596, 'Cheeseburger': 0.26182161942308596, 'Hamburger': 0.20945729553846879, 'Chili Cheese Fries': 0.19200252091026307}
+
 
 #NOTE:Traversal Test 3
 #Same as Traversal Test 2
 #Will make the weights more realistic, with Akshat only bearing "0.7" similarity to Pranav
-# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"])
+# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"], 'traversal_test_3.json')
 #output:
-#Final Current User Menu Weights
-# {'Hot Dog': 10.0, 'Chicken Sandwhich': 3.75, 'Cheeseburger': 3.75, 'Pizza': 3.5, 'Hamburger': 3.0, 'Chili Cheese Fries': 2.75}
+# Final Current User Menu Weights
+#{'Hot Dog': 0.8900511103067338, 'Pizza': 0.8589619180206469, 'Chicken Sandwhich': 0.1976269181812553, 'Cheeseburger': 0.1976269181812553, 'Hamburger': 0.13930051704432594, 'Chili Cheese Fries': 0.12334896247558523}
+
 
 #NOTE:Traversal Test 4
 #Same as Traversal Test 3
 #Let's say Pranav already had pizza, and didn't like it (1.5 star weight)
 #This should decrase it more than there being "just" Akshat's opinion on the pizza being good
-# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"])
+#user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"], 'traversal_test_4.json')
 #output:
 # Final Current User Menu Weights
-# {'Hot Dog': 10.0, 'Pizza': 5.5, 'Chicken Sandwhich': 3.75, 'Cheeseburger': 3.75, 'Hamburger': 3.0, 'Chili Cheese Fries': 2.75}
+#{'Hot Dog': 0.8900511103067338, 'Chicken Sandwhich': 0.1976269181812553, 'Cheeseburger': 0.1976269181812553, 'Pizza': 0.1686255137387421, 'Hamburger': 0.13930051704432594, 'Chili Cheese Fries': 0.12334896247558523}
 
 #NOTE:Traversal Test 5
 #Same as Traversal Test 4
 #Let's say Pranav already had pizza, and kinda liked it (3 star weight)
-#This should be higher than traversal 3, as he's kinda into it and could try it again
+#The's kinda into it and could try it again
 #Pranav still likes hot dog by a whole star above this, so hot dog should be above it
-# user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"])
+#user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"], 'traversal_test_5.json')
 #Final Output:
 # Final Current User Menu Weights
-# {'Hot Dog': 10.0, 'Pizza': 9.25, 'Chicken Sandwhich': 3.75, 'Cheeseburger': 3.75, 'Hamburger': 3.0, 'Chili Cheese Fries': 2.75}
+#{'Hot Dog': 0.9880671556260959, 'Pizza': 0.7736783907595969, 'Chicken Sandwhich': 0.04738051460946453, 'Cheeseburger': 0.04738051460946453, 'Hamburger': 0.019821330271982386, 'Chili Cheese Fries': 0.014759901665130094}
+
+
+#NOTE:Traversal Test 6:
+#Example 2 on poaper
+#Let's say Pranav hasn't had Pizza at Restuaraunt a, but Akshat has had pizza
+#at Restuaraunt B and liked it. Since Akshat and Pranav are 0.7 similar, it should influence his decision
+#to consider Pizza as an option. Since Akshat really liked it, it's likeley that Pizza is a good choice.
+user_item_traversal("NodeID1", ["NodeID2","NodeID3","NodeID4","NodeID5","NodeID6", "NodeID7"], 'traversal_test_6.json')
+# Final Current User Menu Weights
+#{'Hot Dog': 0.9865228106817597, 'Pizza': 0.47679864834946273, 'Chicken Sandwhich': 0.04730645905789546, 'Cheeseburger': 0.04730645905789546, 'Hamburger': 0.01979034961340948, 'Chili Cheese Fries': 0.014736831998877502}
