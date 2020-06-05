92cfe46c29d404e71af98292ef2ba8cc1627646c
Akshat Jain <akshatj@berkeley.edu>
2020-06-04 23:19:01 -0500

    scraped review data from google and yelp. need to do data cleansing

--- server/app.py
+++ server/app.py
@@ -1,17 +1,164 @@
 from google.cloud import vision
 import os, sys, flask, json
 from utils import closest, filterOut, isPrice
+import time
+import pandas as pd
+
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+from urllib.request import urlopen
+from urllib.parse import quote
 
 app = flask.Flask(__name__)
 app.debug = True
 
 client = vision.ImageAnnotatorClient(credentials = json.load(open('choices-545f2b051313.json')))
 
+try: 
+    GOOGLE_KEY = open('google-api-key.txt').read()
+except:
+    sys.exit('Google API Key not found')
 
 MAX_WORD_SPACE = 5
 FILE_NAME = 'image.png'
 
-@app.route("/", methods = ["POST"])
+@app.route("/getReviews", methods=["GET"])
+def getReviews():
+    lat = flask.request.args.get('latitude')
+    lon = flask.request.args.get('longitude')
+
+    dataQuery = {
+        "key": GOOGLE_KEY,
+        "location": f"{lat},{lon}",
+        "type": "food",
+        "rankby": "distance",
+    }
+    try:
+        resp = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?', params=dataQuery)
+        data = resp.json()
+        restaurant = ''
+        for rest in data["results"]:
+            if "cafe" in rest["types"] or "restaurant" in rest["types"] or "food" in rest["types"]:
+                restaurant = rest
+                break
+        
+        rName, rId, rAddress = restaurant["name"], restaurant["place_id"], restaurant["vicinity"]
+
+        googleReviews = getGoogleReviews(rId, rName, rAddress)
+        googleCount = len(googleReviews)
+        yelpCount, yelpReviews = getYelp(rName, rAddress)
+        return flask.jsonify(name=rName, address=rAddress, reviewCount=googleCount+yelpCount, reviews=googleReviews+yelpReviews)
+    except:
+        return flask.jsonify(reviewCount=0, reviews=[])
+
+
+def getGoogleReviews(placeID, name, address):
+    # dataQuery = {
+    #     "key": GOOGLE_KEY,
+    #     "place_id": placeID,
+    # }
+    try:
+        # resp = requests.get("https://maps.googleapis.com/maps/api/place/details/json", params=dataQuery)
+
+        # return resp.json()["result"]["reviews"]
+        driver = webdriver.Chrome('C:/Users/respe/chromedriver')
+
+        driver.get(f'https://www.google.com/search?q={quote(name+" "+address)}')
+        driver.find_element_by_xpath("//a[@data-async-trigger='reviewDialog']").click()
+        time.sleep(2)
+        
+        Reviewer =[]
+        ReviewDate = []
+        ReviewRating =[]
+        ReviewDescription = []
+        TotalReviewsByUser = []
+        thisreview = []
+        last_len = 0
+        def  get_reviews(thisreview):
+            nonlocal last_len
+            for webdriver_obj in thisreview.find_elements_by_class_name("WMbnJf"):
+                Body = webdriver_obj.find_element_by_class_name('Jtu6Td')
+                try:
+                    webdriver_obj.find_element_by_class_name('review-snippet').click()
+                    s_32B = webdriver_obj.find_element_by_class_name('review-full-text')
+                    ReviewDescription.append(s_32B.text)
+                except NoSuchElementException:
+                    if Body.text == '':
+                        return None
+                    ReviewDescription.append(Body.text)
+                Name = webdriver_obj.find_element_by_class_name("TSUbDb")
+                Reviewer.append(Name.text)
+                try:
+                    ReviewByuser = webdriver_obj.find_element_by_class_name("A503be")
+                    TotalReviewsByUser.append(ReviewByuser.text)
+                except NoSuchElementException:
+                    TotalReviewsByUser.append("")
+                star = webdriver_obj.find_element_by_class_name("Fam1ne")
+                ReviewStar =star.get_attribute("aria-label")
+                ReviewRating.append(ReviewStar)
+                Date = webdriver_obj.find_element_by_class_name("dehysf")
+                ReviewDate.append(Date.text)
+                    
+                element = webdriver_obj.find_element_by_class_name('PuaHbe')
+                driver.execute_script("arguments[0].scrollIntoView();", element)
+            time.sleep(1)
+            reviews = driver.find_elements_by_class_name("gws-localreviews__general-reviews-block")
+            r_len = len(reviews)
+            if r_len > last_len:
+                last_len = r_len
+                get_reviews(reviews[r_len-1])
+        
+        reviews = driver.find_elements_by_class_name("gws-localreviews__general-reviews-block")
+        last_len = len(reviews)
+        get_reviews(reviews[last_len - 1])
+    
+        data = pd.DataFrame ( { 'author' : Reviewer,
+                                'rating': ReviewRating,
+                                'description': ReviewDescription})
+        driver.quit() 
+        return data.to_dict('records')
+    except Exception as inst:
+        print(inst)
+        return []
+   
+
+def getYelp(restaurantName, location):
+    page = urlopen("https://www.yelp.com/search?find_desc=" + quote(restaurantName) + "&find_loc=" + quote(location))
+    soup = BeautifulSoup(page, features="html.parser")
+
+    restLink = ''
+    for link in soup.findAll('a', href=True):
+        if 'osq' in link['href']:
+            restLink = link['href']
+            break
+
+    if restLink != '':
+        def extractYelpReviews(relativeUrl): 
+            url = 'https://yelp.com' + relativeUrl
+
+            soup = BeautifulSoup(urlopen(url), features="html.parser")
+            scripts = soup.findAll("script", attrs={'type': 'application/ld+json'})
+            reviewDict = json.loads(scripts[-1].text)
+
+            return reviewDict
+
+        d = extractYelpReviews(restLink)
+        reviews = d['review']
+        reviewCount = d['aggregateRating']['reviewCount']
+
+        for i in range(20, reviewCount, 20):
+            reviews.extend(extractYelpReviews(restLink + '&start=' + str(i))['review'])
+        
+        return reviewCount, reviews
+    else:
+        return 0, []
+    
+
+@app.route("/parseMenu", methods = ["POST"])
 def parseMenu():
     content = str.encode(flask.request.json['data'])
     print(content, file=sys.stderr)
@@ -95,7 +242,7 @@ def parseMenu():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', threaded=True, debug=True)
 
     #
     # web_response = client.web_detection(image=image)
